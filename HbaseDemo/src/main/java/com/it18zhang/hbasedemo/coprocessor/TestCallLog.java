19ac7c4a1ca5131b6750e88300b9d24d6b2335b3
yunspirit <cloudwill@163.com>
2019-12-02 00:47:24 +0800

    bug fixed

---
+++ HbaseDemo/src/main/java/com/it18zhang/hbasedemo/coprocessor/TestCallLog.java
@@ -0,0 +1,53 @@
+package com.it18zhang.hbasedemo.coprocessor;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.Connection;
+import org.apache.hadoop.hbase.client.ConnectionFactory;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.client.Table;
+
+import java.io.IOException;
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class TestCallLog {
+
+
+    public static void main(String[] args) throws IOException {
+
+        Configuration conf = HBaseConfiguration.create();
+        Connection conn = ConnectionFactory.createConnection(conf);
+        TableName tname = TableName.valueOf("ns1:calllogs");
+        Table table = conn.getTable(tname);
+        String callerId = "13845456767";
+        String calleeId = "139898987878";
+        SimpleDateFormat sdf = new SimpleDateFormat();
+        sdf.applyPattern("yyyyMMddHHmmss");
+        String callTime = sdf.format(new Date());
+        int duration = 100;
+        DecimalFormat dff = new DecimalFormat();
+        dff.applyPattern("00000");
+        String durStr = dff.format(duration);
+        //区域00-99
+        int hash = (callerId + callTime.substring(0, 6)).hashCode();
+        hash =(hash & Integer.MAX_VALUE)%100;
+        //hash区域号
+        DecimalFormat df = new DecimalFormat();
+        df.applyPattern("00");
+        String regNo = df.format(hash);
+        //拼接rowkey
+        //xx , callerid , time , direction, calleid ,duration
+        String rowkey = regNo + "," + callerId + "," + callTime + "," + "0," + calleeId + "," + durStr;
+        byte[] rowid = Bytes.toBytes(rowkey);
+        Put put = new Put(rowid);
+        put.addColumn(Bytes.toBytes("f1"),Bytes.toBytes("callerPos"),Bytes.toBytes("河北"));
+        put.addColumn(Bytes.toBytes("f1"),Bytes.toBytes("calleePos"),Bytes.toBytes("河南"));
+        //执行插入
+        table.put(put);
+        System.out.println("over");
+    }
+
+}
